11074348c5bf803519367c9f1cf583e9d86ac003
Johan <johanbp27@gmail.com>
2024-10-23 23:44:48 -0400

    trying something else

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Hours } from "./pages/Hours";
 import { Team } from "./pages/Team";
 import { Services } from "./pages/Services";
 import { RootLayout } from "./layout/RootLayout";
+import { NotFound } from "./pages/NotFound"; // Ensure you import this
+
 import {
   Route,
   createBrowserRouter,
@@ -14,14 +16,16 @@ import {
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<RootLayout />} basename="/whmc-website">
         <Route index element={<Home />} />
         <Route path="services" element={<Services />} />
         <Route path="our-team" element={<Team />} />
         <Route path="hours" element={<Hours />} />
         <Route path="plan-a-visit" element={<PlanAVisit />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
-    )
+    ),
+    console.log("App.jsx has been loadded")
   );
   return <RouterProvider router={router} />;
 }
--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -2,15 +2,7 @@ export const Hours = () => {
   return (
     <main className="mt-[4rem] flex flex-col place-items-center py-20 text-darkGreen">
       <h1 className="font-ubuntu font-bold text-6xl">Our Hours.</h1>
-      <table>
-        <th>Hours</th>
-        <tbody>
-          <td>Once</td>
-          <td>twice</td>
-          <td>twice</td>
-          <td>twice</td>
-        </tbody>
-      </table>
+      <table></table>
     </main>
   );
 };
