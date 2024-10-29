dd20d643da10286b2e67af5e0e1a68d7af8e5c36
Johan <johanbp27@gmail.com>
2024-10-29 12:54:00 -0400

    completed hours page

---
+++ src/components/CustomMap.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+
+export const CustomMap = () => {
+  const MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const MAP_ID = import.meta.env.VITE_MAP_ID;
+  const mapParams = {
+    zoom: 17,
+    center: { lat: 43.8593729542859, lng: -79.04016655890393 },
+    MAP_ID: MAP_ID,
+  };
+
+  return (
+    <APIProvider
+      apiKey={MAP_API_KEY}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <Map
+        defaultZoom={mapParams.zoom}
+        defaultCenter={mapParams.center}
+        mapId={mapParams.MAP_ID}
+        disableDefaultUI={true}
+        zoomControl={true}
+      >
+        <AdvancedMarker position={mapParams.center}>
+          <Pin
+            background={"#7ca982"}
+            glyphColor={"#243e36"}
+            borderColor={"#f1f7ed"}
+          />
+        </AdvancedMarker>
+      </Map>
+    </APIProvider>
+  );
+};
--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -1,10 +1,73 @@
-import { Calendar } from "../components/Calendar";
+import { FaLocationDot, FaPhone } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+import { CustomMap } from "../components/CustomMap";
 
 export const Hours = () => {
   return (
-    <main className="mt-20 flex flex-col place-items-center py-20 text-darkGreen">
-      <h1 className="font-ubuntu font-bold text-6xl">Our Hours.</h1>
-      <Calendar />
+    <main className="mt-20 flex flex-col place-items-center py-20 px-10 text-darkGreen font-serif">
+      <h1 className="font-ubuntu font-bold text-6xl mb-20">Our Hours.</h1>
+      <section className="grid grid-cols-2 w-4/5">
+        <div className="flex flex-col gap-2 bg-lightGreen text-white p-10">
+          <h2 className="font-ubuntu text-4xl font-bold">Contact Us</h2>
+          <p className="flex place-items-center">
+            <FaLocationDot className="mr-2 size-5" />
+            15 Westney Road N &#35;2, Ajax, ON, L1T 1P4{" "}
+          </p>
+          <p className="flex place-items-center">
+            <FaPhone className="mr-3" />
+            905&#45;428&#45;8055
+          </p>
+          <p className="flex place-items-center">
+            <IoMail className="mr-3" />
+            management.westeyheights@gmail.com
+          </p>
+          <p className="font-bold text-xl my-4">Visits are by walk-in only.</p>
+          <p>
+            Patients are seen Monday through Sunday starting at 8am. Closing
+            times may vary from posted hours depending on clinic capacity and
+            wait times.
+          </p>
+          <p>
+            Patients can contact the clinic to find out the exact closing time
+            and wait times for each day.
+          </p>
+          <h2 className="font-ubuntu text-4xl font-bold">Hours</h2>
+          <div className="flex flex-row">
+            <div className="flex flex-col">
+              <p className="my-1">Monday</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">Tuesday</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">Wednesday</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">Thursday</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">Friday</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">Weekends</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+            </div>
+            <div className="text-right flex flex-col">
+              <p className="my-1">8am - 4pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">8am - 2pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">8am - 4pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">8am - 4pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">8am - 2pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+              <p className="my-1">8am - 2pm</p>
+              <hr className="border-opacity-75 border-darkGreen w-40" />
+            </div>
+          </div>
+          {/*<Calendar />*/}
+        </div>
+        <div className="">
+          <CustomMap />
+        </div>
+      </section>
     </main>
   );
 };
