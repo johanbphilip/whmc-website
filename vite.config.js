9c2881b6ce857e02ba577e83edaaa265946b41da
Johan <johanbp27@gmail.com>
2025-01-23 19:51:54 -0500

    trying to commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/whmc-website", //repo name for gh-pages
+  // base: "/whmc-website", //repo name for gh-pages
   plugins: [react()],
 });
